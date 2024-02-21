64e465e5b88189492a97d0c374c7874da6b5d21d
alecsandva <153679467+alecsandva@users.noreply.github.com>
2024-02-21 15:15:39 +0000

    Add files via upload

---
+++ replace.py
@@ -0,0 +1,8 @@
+#replace.py
+string1 = "The!quick!brown!fox!jumps!over!the!lazy!dog."
+new_string = string1.replace("!", " ")
+print(new_string)
+upper_string = new_string.upper
+print(upper_string)
+reverse_string = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG"[::-1]
+print(reverse_string)
