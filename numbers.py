04bf4965944bfda74996dda7e792c8e1c40cbde0
alecsandva <153679467+alecsandva@users.noreply.github.com>
2024-02-28 21:17:32 +0000

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,21 @@
+#Get 3 integers from a user to perform calculations
+num1 = int(input("Enter a whole number: "))
+num2 = int(input("Enter another whole number: "))
+num3 = int(input("Enter one more whole number: "))
+print(num1 + num2 + num3)
+
+#Calculate the sum of the nrs
+sum_numbers = num1 + num2 + num3
+print(sum_numbers)
+
+#Calculate first nr minus second nr
+subtraction_numbers = num1 - num2
+print(subtraction_numbers)
+
+#Calculate third nr multiplied by first nr
+multi_numbers = num1 * num3
+print(multi_numbers)
+
+#Calculate sum of all three nr sdivided by third nr
+division_numbers = sum_numbers / num3 
+print(division_numbers)
