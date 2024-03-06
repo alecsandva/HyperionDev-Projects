a269396520b6551fa8258bf7cf17444fc3430bb0
Alexandra Munteanu <153679467+alecsandva@users.noreply.github.com>
2024-03-06 23:59:09 +0000

    Add files via upload

--- replace.py
+++ replace.py
@@ -1,8 +1,17 @@
-#replace.py
-string1 = "The!quick!brown!fox!jumps!over!the!lazy!dog."
-new_string = string1.replace("!", " ")
+# Replace the "!" with " " 
+
+string_1 = "The!quick!brown!fox!jumps!over!the!lazy!dog."
+new_string = string_1.replace("!", " ")
 print(new_string)
-upper_string = new_string.upper
+
+# Print new string in upper case
+
+upper_string = new_string.upper()
 print(upper_string)
-reverse_string = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG"[::-1]
-print(reverse_string)
+
+# Print strings in reverse
+
+reverse_string_1 = new_string[::-1]
+print(reverse_string_1)
+reverse_string_2 = upper_string[::-1]
+print(reverse_string_2)
