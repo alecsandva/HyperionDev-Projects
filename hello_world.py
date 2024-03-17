885b29b828f87667aa9b2bf162a4bd3cf92818fd
Alexandra Munteanu <153679467+alecsandva@users.noreply.github.com>
2024-03-17 22:40:51 +0000

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,7 @@
+#First create a variable for name to receive input from a user
+#Create a variable for age to receive input from user, while also converting the numerical value to a string
+#Print out the strings to display in console
+name = input("Please enter your name:")
+age  = str(int(input("Please enter your age: ")))
+print("Hello " + name + " " + "you are "+ age + "years old.")
+print("Hello, world!")
